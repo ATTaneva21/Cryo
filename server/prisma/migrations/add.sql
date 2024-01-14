7dfc3e78530b12c91af67a713787d49a98ea14d7
ATTaneva21 <ATTaneva21@codingburgas.bg>
2024-01-14 16:32:57 +0200

    Add creation of Transaction and Card table

--- server/prisma/migrations/add.sql
+++ server/prisma/migrations/add.sql
@@ -7,6 +7,9 @@ CREATE TABLE "User" (
     "dateOfBirth" TEXT NOT NULL,
     "email" TEXT NOT NULL,
     "password" TEXT NOT NULL,
+    "balance" INT NOT NULL,
+    "residency" TEXT NOT NULL,
+    "socialSecurityNumber" TEXT NOT NULL,
 
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
@@ -14,22 +17,31 @@ CREATE TABLE "User" (
 --CreateTable
 CREATE TABLE "Will" (
     "id" SERIAL NOT NULL,
+    "type" TEXT NOT NULL,
+    "cardUsed" TEXT NOT NULL,
     "firstName" TEXT NOT NULL,
-    "surname" TEXT NOT NULL,
     "lastName" TEXT NOT NULL,
-    "dateOfBirth" TEXT NOT NULL,
-    "email" TEXT NOT NULL,
-    "password" TEXT NOT NULL,
+    "type" TEXT NOT NULL,
+    "addressOrBrand" TEXT NOT NULL,
 );
 
 --CreateTable
-CREATE TABLE "Transactions" (
+CREATE TABLE "Transaction" (
     "id" SERIAL NOT NULL,
-    "firstName" TEXT NOT NULL,
-    "surname" TEXT NOT NULL,
+    "companyName" TEXT NOT NULL,
+    "amount" INT NOT NULL,
     "lastName" TEXT NOT NULL,
-    "dateOfBirth" TEXT NOT NULL,
-    "email" TEXT NOT NULL,
-    "password" TEXT NOT NULL,
+    "cardUsed" INT NOT NULL,
+    "typeOfCard" TEXT NOT NULL,
+    "purchaseStatus" TEXT NOT NULL,
+);
+
+--CreateTable
+CREATE TABLE "Card" (
+    "id" SERIAL NOT NULL,
+    "cardNumber" TEXT NOT NULL,
+    "CVV" TEXT NOT NULL,
+    "dateValid" TEXT NOT NULL,
+    "nameOnCard" TEXT NOT NULL,
 );
 
